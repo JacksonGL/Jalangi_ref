fac9429d63ff8453851663f1df4e7b3eaa738896
Liang Gong <jacksongl1988@gmail.com>
2013-11-13 18:47:08 -0800

    new update by Liang Gong @ Wed Nov 13 18:47:08 PST 2013

--- analysis.js
+++ analysis.js
@@ -1969,36 +1969,58 @@
 
 // check NaN
     J$.analyzer = {
-        pre_B: function (iid, op, left, right) {
-            //return result_c;
+        // G: get field
+        // function called before G
+        // base is the object from which the field will get
+        // offset is either a number or a string indexing the field to get
+        pre_G: function (iid, base, offset, norr) {
+            //if((iid == 306509 || iid == 306517)  && (isNaN(base[offset]))) {
+            //    console.log('pre get [iid: ' + iid +']:' + base[offset] + ':' + (typeof base[offset]));
+            //}
         },
-        post_B: function (iid, op, left, right, val) {
-            if(((this.isMeaningless(left) || this.isMeaningless(right)) && op != '==' && op != '!=' && op != '===' && op != '!==' && op != 'instanceof' && op != 'in' && op != '&&' && op != '||') 
-                || typeof val == 'undefined' ||  ((typeof val == 'number') && isNaN(val) == true)) {
-                console.warn('[strange binary operation: | iid: ' + iid +']:' + val);
-                console.group();
-                console.warn('left: ' + left + '[' + typeof left +']' + '  op:' + op + '  right: ' + right + '[' + typeof right +']');
-                this.info();
-                console.groupEnd();
+        // G: get field
+        // function called after G
+        // base is the object from which the field will get
+        // offset is either a number or a string indexing the field to get
+        // val is the value gets from base.[offset]
+        // return value will affect the retrieved value in the instrumented code
+        post_G: function (iid, base, offset, val, norr) {
+            //if((iid == 306509 || iid == 306517)  && (isNaN(val))) {
+            //    console.log('[iid: ' + iid +']:' + val + ':' + (typeof val));
+            //}
+            try{
+                if(typeof base != 'undefined' && base != null && (typeof val == 'number') && isNaN(val) == true){
+                    console.log('[NaN iid: ' + iid +'] ' + base + '.' + offset + ':' + val);
+                    this.info(base);
+                }
+            }catch(e){
+                console.log(e);
             }
             return val;
-            //return result_c;
         },
-        U: function (iid, op, left) {
-
-            //return result_c;
+        // P: put field
+        // function called before P
+        // base is the object to which the field will put
+        // offset is either a number or a string indexing the field to get
+        // val is the value puts to base.[offset]
+        pre_P: function (iid, base, offset, val) {
+            //return val;
         },
-        C1: function (iid, left) {
-            //var left_c;
-            //return left_c;
-        },
-        C2: function (iid, left) {
-            //var left_c, ret;;
-            //return left_c;
-        },
-        C: function (iid, left) {
-            //var left_c, ret;
-            //return left_c;
+        // P: put field
+        // function called after P
+        // base is the object to which the field will put
+        // offset is either a number or a string indexing the field to get
+        // val is the value puts to base.[offset]
+        // return value will affect the retrieved value in the instrumented code
+        post_P: function (iid, base, offset, val) {
+            if(typeof base != 'undefined' && base != null && (typeof val == 'number') && isNaN(val) == true){
+                console.warn('[NaN iid: ' + iid +'] ' + base + '.' + offset + ' <= ' + val);
+                this.info(base);
+            } else if (typeof base != 'undefined' && base != null && (typeof val == 'undefined')) {
+                console.warn('[undefined iid: ' + iid +'] ' + base + '.' + offset + ' <= ' + typeof val);
+                this.info(base);
+            }
+            return val;
         },
         info: function (obj) {
             console.groupCollapsed();
@@ -2014,7 +2036,7 @@
             } else if(typeof val == 'number' && isNaN(val)){
                 return true;
             }
-            return false;    
+            return false;   
         }
     };
 
