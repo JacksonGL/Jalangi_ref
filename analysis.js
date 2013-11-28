b1e12116107100c8150e05447a450ac6001dd5b9
Liang Gong <jacksongl1988@gmail.com>
2013-11-27 16:17:48 -0800

    new update by Liang Gong @ Wed Nov 27 16:17:48 PST 2013

--- analysis.js
+++ analysis.js
@@ -2260,3 +2260,79 @@ J$.listAPI = function () {
     };
 
 J$.analyzer = undefined;
+
+
+
+
+J$.type_memo = [];
+J$.analysis = {
+    getField: function(iid, base, offset, val) {
+        if(base){
+            if(base.__proto__ && base.__proto__.constructor && base.__proto__.constructor.name && base.__proto__.constructor.name == 'Array'){
+                return val;
+            }
+
+            if(typeof offset == 'number') {
+                return val;
+            }
+
+            if(typeof base != 'object') {
+                return val;
+            }
+            var iOffset;
+            var shouldReturn = true;
+            try{
+                iOffset = parseInt(offset)
+            } catch(e){
+                shouldReturn = false;
+            }
+
+            if(shouldReturn===true && !isNaN(iOffset)){
+                return val;
+            }
+
+            if(J$.type_memo){
+                var signature  = '';
+                for (var prop in base) {
+                    if (base.hasOwnProperty(prop)) {
+                        signature += prop + '|';
+                    }
+                }
+                if(J$.type_memo[iid]){
+                    outter:{
+                        for(var i=0;i<J$.type_memo[iid].length;i++){
+                            if(J$.type_memo[iid][i] == signature){
+                                break outter;
+                            }
+                        }
+                        J$.type_memo[iid].push(signature);
+                    }
+                } else {
+                    J$.type_memo[iid] = [];
+                    J$.type_memo[iid].push(signature);
+                }
+            } else {
+                J$.type_memo = [];
+            }
+        }
+        
+        return val;
+    }
+};
+
+J$.typeInfo = function() {
+        if(J$.type_memo){
+            for(var i=0;i<J$.type_memo.length;i++){
+                if(J$.type_memo[i] && J$.type_memo[i].length > 1){
+                    console.log('iid: ' + i + ':');
+                    console.group();
+                    for(var j=0;j<J$.type_memo[i].length;j++){
+                        console.log('sig['+j+']:' + J$.type_memo[i][j]);
+                    }
+                    console.groupEnd();
+                }
+            }
+        } else {
+            J$.type_memo = [];
+        }
+    }
