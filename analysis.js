bb5817143dc26ef62d1513a1631d1237e7981456
Liang Gong <jacksongl1988@gmail.com>
2013-12-01 17:50:21 -0800

    new update by Liang Gong @ Sun Dec  1 17:50:21 PST 2013

--- analysis.js
+++ analysis.js
@@ -2344,6 +2344,11 @@ J$.typeInfo = function() {
 
 // try to find x === NaN or x == NaN operation
 J$.analysis = {
+    getFieldPre: function (iid, base, offset, val) {
+        if(offset === 'refcount'){
+            console.log(val + '[type: ' + typeof val + ']');
+        }
+    },
     binaryPre: function (iid, op, left, right) {
         if(op === '==' || op == '===') {
             if(left !== left || right !== right) {
