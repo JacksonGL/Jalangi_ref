25c8f32fc3f591dff3225208f22eea3a2fb78956
Liang Gong <jacksongl1988@gmail.com>
2014-01-13 21:47:26 -0800

    new update by Liang Gong @ Mon Jan 13 21:47:26 PST 2014

--- analysis.js
+++ analysis.js
@@ -2767,9 +2767,9 @@ J$.analysis = {
 
 J$.console = {
     
-}
+};
 
-J$.console.log = function(str) {
+J$.output = function(str) {
     console.log(str); 
     //window.postMessage(str);
 };
@@ -2779,41 +2779,41 @@ J$.console.log = function(str) {
      J$.analysis = {
         putFieldPre: function (iid, base, offset, val) {
             if (typeof base === 'boolean' || typeof base === 'number' || typeof base === 'string') {
-                J$.console.log('[iid: ' + iid + '] setting property [' + offset + '] of base object: ' + typeof base);
+                J$.output('[iid: ' + iid + '] setting property [' + offset + '] of base object: ' + typeof base);
             }
 
             if (offset === '__proto__') {
-                J$.console.log('[iid: ' + iid + '] setting property [' + offset + '] of base object: ' + typeof base);
+                J$.output('[iid: ' + iid + '] setting property [' + offset + '] of base object: ' + typeof base);
             }
             return val;
         },
         getFieldPre: function (iid, base, offset) {
             if(typeof base === 'string'){
                 if(/[\uD800-\uDFFF]/.test(base) && (offset === 'length' || offset === 'charAt' || offset === 'charCodeAt')) {
-                    J$.console.log('[iid: ' + iid + '] getting property [' + offset + '] of string containing surrogate pair: ' + base);
+                    J$.output('[iid: ' + iid + '] getting property [' + offset + '] of string containing surrogate pair: ' + base);
                 }
             }
         },
         invokeFunPre: function (iid, f, base, args, isConstructor) {
             if(f===J$.variables.concat && args[0] && args[0].callee && args[0].length) {
-                J$.console.log(args);
-                J$.console.log('[iid: ' + iid + '] calling concat function with arguments');
+                J$.output(args);
+                J$.output('[iid: ' + iid + '] calling concat function with arguments');
             } 
         },
         binaryPre: function (iid, op, left, right) {
             if(typeof left === 'string' && typeof right === 'object' && right !== null && right.__proto__ === Object.prototype) {
                 if (left == right){
-                    J$.console.log('[iid: ' + iid + '] string == object (===)');
+                    J$.output('[iid: ' + iid + '] string == object (===)');
                 }
             } else if (typeof right === 'string' && typeof left === 'object' && left !== null && left.__proto__ === Object.prototype) {
                 if (left == right){
-                    J$.console.log('[iid: ' + iid + '] string == object (===)');
+                    J$.output('[iid: ' + iid + '] string == object (===)');
                 }
             } 
         },
         readPre: function (iid, name, val, isGlobal) { 
             if(name === 'this' && val === window) {
-                J$.console.log('[iid: ' + iid + '] this===window'); 
+                J$.output('[iid: ' + iid + '] this===window'); 
             }
         }
     }; 
