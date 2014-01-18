766ed76c719a3e1383bf1648f49544adbd44ec81
Liang Gong <jacksongl1988@gmail.com>
2014-01-18 15:30:50 -0800

    new update by Liang Gong @ Sat Jan 18 15:30:50 PST 2014

--- analysis.js
+++ analysis.js
@@ -2379,7 +2379,7 @@ J$.susp_num = 0;
                 
             }
 
-            if(typeof left !== typeof right && && op != '==' && op != '!=' && op != '===' && op != '!==' && op != 'instanceof' && op != 'in' && op != '&&' && op != '||') {
+            if(typeof left !== typeof right && op != '==' && op != '!=' && op != '===' && op != '!==' && op != 'instanceof' && op != 'in' && op != '&&' && op != '||') {
                 console.warn('[strange binary operation: | iid: ' + iid +']:' + val);
                 console.group();
                 console.warn('left: ' + left + '[' + typeof left +']' + '  op:' + op + '  right: ' + right + '[' + typeof right +']');
